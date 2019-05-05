ce574b4958bdb2d3f7270cf37f62200d84e09c1b
Blake Rose <bdecktor@gmail.com>
2019-05-05 01:59:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,6 @@ def batch_badge_creator(badge_maker)
   participants = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   participants.each do |config|
     puts "Hello, my name is #{config}."
-    batch_badge_creator +=1 
-  end 
-end 
-
-
-
-
-
+    batch_badge_creator += 1 
+  end
+end
