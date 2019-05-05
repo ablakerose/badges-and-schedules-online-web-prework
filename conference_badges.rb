c4ee40966570ebbfecf0e7874ce9a11143c5fc50
Blake Rose <bdecktor@gmail.com>
2019-05-05 02:53:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,12 @@ end
 
 def batch_badge_creator(array_names)
   array_names.each do |name|
-    return "Hello, my name is #{name}."
+    puts "Hello, my name is #{name}."
     batch_badge_creator += 1
-    end
+  end
 end
+
+def assign_rooms ()
+
+  return "Hello, #{name}! You'll be assigned to room #{room}!"
+  
