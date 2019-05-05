9a31660308d75dd10929a11927c941c9bc2a1cc9
Blake Rose <bdecktor@gmail.com>
2019-05-05 03:37:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms (array_names)
   array_welcome_rooms= []
   array_names.each do |name|
-    array_welcome_rooms << "Hello, #{name}! You'll be assigned to room #{name.index}!"
+    array_welcome_rooms << "Hello, #{name}! You'll be assigned to room #{name.index(1)}!"
   end
   return array_welcome_rooms
 end
