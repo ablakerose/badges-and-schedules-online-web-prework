6d89424cba0db787577cc0adae5e14c5a61f116b
Blake Rose <bdecktor@gmail.com>
2019-05-05 04:04:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,5 +15,5 @@ def assign_rooms(array_names)
   array_names.each_with_index do |name, index|
     array_welcome_rooms << "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
-
+  return array_welcome_rooms
 end
