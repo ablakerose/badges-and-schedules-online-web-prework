b53dcd3194198698e90bb0d730c4e84c244c220d
Blake Rose <bdecktor@gmail.com>
2019-05-05 02:07:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,10 @@
-def conference_badges(badge_maker)
+def badge_maker(name)
   badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badge_maker.each do |name|
     puts "Hello, my name is #{name}."
     conference_badges += 1
   end
 end
+
+def assign_rooms() 
+  
