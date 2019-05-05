8a9fcdd885892cdddd33a515c559527abf315eb4
Blake Rose <bdecktor@gmail.com>
2019-05-05 02:23:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,3 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
   name=Arel
 end
-
-def badge_maker_creator(array_names)
-  array_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  array_names.each do |name|
-    puts "Hello, my name is #{name}."
-    badge_maker_creator += 1
-  end
